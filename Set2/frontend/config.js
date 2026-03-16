e44131673d6d57df9b6dca47cc71013d5dfc0747
Istavan <istavan@Admins-iMac.local>
2026-03-16 15:27:40 +0700

    ซ่อม frontend

--- Set2/frontend/config.js
+++ Set2/frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL: 'https://YOUR_AUTH_SERVICE.up.railway.app',
-  TASK_URL: 'https://YOUR_TASK_SERVICE.up.railway.app',
-  USER_URL: 'https://YOUR_USER_SERVICE.up.railway.app'
+  AUTH_URL: 'http://localhost:3001',
+  TASK_URL: 'http://localhost:3002',
+  USER_URL: 'http://localhost:3003'
 };
